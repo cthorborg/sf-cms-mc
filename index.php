0dfcb68e61efe99ae70dd7fb358d91dc3414d17d
cthorborg <cthorborg@live.com.au>
2019-04-18 17:31:53 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php echo '<p>Hello World</p>'; ?> 
 <iframe style="height:600px;width:600px;" src="https://sfmc.stylelabsdev.com/en-us/sitecore-plugin/approved-assets"></iframe>
+
+<script src="https://apis.google.com/js/api.js"></script>
+<script>
+  window.addEventListener("message", function(event){alert(JSON.stringify(event));}, false);
+</script>
