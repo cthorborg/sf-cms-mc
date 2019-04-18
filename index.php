110bd501b0a011cdd1a6ac2e7e2b7bdda99a49ff
cthorborg <cthorborg@live.com.au>
2019-04-18 17:40:17 +1000

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,37 @@
 <script src="https://apis.google.com/js/api.js"></script>
 <script>
   window.addEventListener("message", function(event){
-    if(event.__type)
-      alert(JSON.stringify(event));
+    if(event.data.__type)
+      alert(JSON.stringify(event.data));
   }, false);
-</script>
+
+function chosen(imgId) {
+
+  var sdk = new window.sfdc.BlockSDK(); //initalize SDK
+  sdk.setContent(""); //resets content block
+
+  var fit = document.getElementById('scaleFit').checked;
+  var acenter = document.getElementById('alignCenter').checked;
+
+  selectedImageId = imgId;
+
+  if(imgId != ""){
+    image = document.getElementById(imgId).src;
+  }
+
+  if(fit){
+    sdk.setContent("<img width='100%' src='" + image + "'/>");
+
+  }else{
+
+    if(acenter){
+        sdk.setContent("<center><img src='" + image + "'/></center>");
+
+      }else{
+      sdk.setContent("<img src='" + image + "'/>");
+
+    }
+  }
+}
+
+    </script>
