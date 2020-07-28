2b0732a70c83c49eb1fc5d0051fd8ee0bc4888bc
Christopher Thorborg <cthorborg@cthorborg-ltm.internal.salesforce.com>
2020-07-28 14:42:09 +1000

    Adding consol logs to find error

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
   <script src="https://apis.google.com/js/api.js"></script>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -83,13 +83,12 @@
 
             originals = json_data;
 
-          	console.log(data);
-          	console.log(originals);
-        	  console.log(base_url.concat(originals.items[0].contentNodes.bannerImage.url));
+          	console.log("data: " + data);
+          	console.log("origionals: " + originals);
+        	  console.log("test url:" + base_url.concat(originals.items[0].contentNodes.bannerImage.url));
 
           	for (i in json_data.items) {
-          		console.log(i);
-              alert(i);
+          		console.log("i:" + i);
           		$("#list-tab").append("<a class=\"list-group-item list-group-item-action ctstyle itemlist\" id=\"list-home-list\" data-toggle=\"list\" href=\"#list-home\" role=\"tab\" aria-controls=\"home\" onclick=\"javascript:chosen('" + i + "')\"><image width=\"20%\" id=\"image" + "1" + "\" src=\""+base_url.concat(i.contentNodes.bannerImage.url)+"\" /> <span style=\"padding-left: 20px\">"+i.contentNodes.bannerImage.altText+"</span></a>")
           	}
           }});
